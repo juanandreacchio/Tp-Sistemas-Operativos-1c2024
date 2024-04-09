8c0399c380c88b8c29f3772462d92d620b6be51f
Matias-Dopazo <mdopazo@frba.utn.edu.ar>
2024-04-09 21:28:10 +0000

    funciona el handshake entre cpu y memoria

--- memoria/src/main.c
+++ memoria/src/main.c
@@ -4,17 +4,19 @@ t_log *logger_memoria;
 t_config *config_memoria;
 
 char *puerto_memoria;
+char* mensaje_recibido;
 
 int main(int argc, char* argv[]) {
+    //logger memoria
     logger_memoria = iniciar_logger("config/memoria.log", "Memoria", LOG_LEVEL_INFO);
 
+    //servidor de cpu
     iniciar_config();
-
-    int socket_servidor_memoria = iniciar_servidor(logger_memoria, puerto_memoria, "Memoria");
+    int socket_servidor_memoria = iniciar_servidor(logger_memoria, puerto_memoria, "Cpu");
     int socket_cliente = esperar_cliente(socket_servidor_memoria, logger_memoria);
+
     
     t_list* lista;
-    while (1) {
         int cod_op = recibir_operacion(socket_cliente);
 
         switch (cod_op) {
@@ -33,7 +35,13 @@ int main(int argc, char* argv[]) {
                 log_warning(logger_memoria, "Operacion desconocida");
                 break;
         }
-    }
+    
+    //TODO
+    //servidor de kernel
+    //servidor de interfaz de I/O
+    
+    
+    
 
 
     terminar_programa(socket_servidor_memoria, logger_memoria, config_memoria);
